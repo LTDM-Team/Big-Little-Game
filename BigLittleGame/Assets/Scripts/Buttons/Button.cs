ec4dbbbbbbe8f9f831f0976961cbdbf5cc25ab51
Taptrue1 <82027635+Taptrue1@users.noreply.github.com>
2021-07-03 18:26:17 +0300

    Добавил кнопки

---
+++ BigLittleGame/Assets/Scripts/Buttons/Button.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _sizeToEnable;
+    [SerializeField]
+    private GameObject[] _doors;
+    private bool _isActive = false;
+
+    private SpriteRenderer ButtonRenderer;
+    private void Awake()
+    {
+        ButtonRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if(!_isActive && player != null && player.Size == _sizeToEnable)
+        {
+            ButtonRenderer.color = new Color(61, 255, 66, 255);
+            DisactivateDoors();
+            _isActive = true;
+        }
+        Debug.Log("Кнопка нажата");
+    }
+    private void DisactivateDoors()
+    {
+        foreach(GameObject door in _doors)
+        {
+            door.SetActive(false);
+        }
+    }
+}
