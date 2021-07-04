31fa575de95a3ad68af431f71ef9ff0d1e6c2ec8
Taptrue1 <82027635+Taptrue1@users.noreply.github.com>
2021-07-04 16:28:22 +0300

    Исправил финиш

--- BigLittleGame/Assets/Scripts/LoadNewLevel.cs
+++ BigLittleGame/Assets/Scripts/LoadNewLevel.cs
@@ -4,8 +4,6 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class LoadNewLevel : MonoBehaviour
 {
-    [SerializeField]
-    private int _indexOfSceneToLoad;
     private void Awake()
     {
         BoxCollider2D finishCollider = GetComponent<BoxCollider2D>();
@@ -16,7 +14,8 @@ public class LoadNewLevel : MonoBehaviour
         Player player = collision.GetComponent<Player>();
         if(player != null)
         {
-            SceneManager.LoadScene(_indexOfSceneToLoad);
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
         }
     }
 }
