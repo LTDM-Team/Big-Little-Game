538d4d988bda9e4b652dbdd18d5b2906e497eeda
JustLink <linki_mail@mail.ru>
2021-07-04 16:22:02 +0700

    Поставлены новые спрайты на кнопку, немного переделана активация кнопки, поправлен коллайдер дверей, поправлен коллайдер порталов

--- BigLittleGame/Assets/Scripts/Buttons/Button.cs
+++ BigLittleGame/Assets/Scripts/Buttons/Button.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     [SerializeField]
-    private float _sizeToEnable;
+    private float _minSizeToEnable;
     [SerializeField]
     private GameObject[] _doors;
     private bool _isActive = false;
@@ -16,13 +16,14 @@ public class Button : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.gameObject.GetComponent<Player>();
-        if(!_isActive && player != null && player.Size.y >= _sizeToEnable)
+        if(!_isActive && player != null && player.Size.y >= _minSizeToEnable)
         {
-            ButtonRenderer.color = new Color(61, 255, 66, 255);
+            // ButtonRenderer.color = new Color(61, 255, 66, 255);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 2, transform.localScale.z);
+
             DisactivateDoors();
             _isActive = true;
         }
-        Debug.Log("Кнопка нажата");
     }
     private void DisactivateDoors()
     {
