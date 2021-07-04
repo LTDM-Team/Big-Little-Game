21501ff2c9325ac8f6a4f5730d3c39c21f4b5d29
JustLink <linki_mail@mail.ru>
2021-07-04 16:51:51 +0700

    Поправлен LevelReloader

--- BigLittleGame/Assets/Scripts/LevelReloader.cs
+++ BigLittleGame/Assets/Scripts/LevelReloader.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelReloader : MonoBehaviour
 {
-    [SerializeField]
-    private string _sceneName;
-    [SerializeField]
-    private KeyCode _reloadKey;
+    [SerializeField] private KeyCode _reloadKey;
+
     private void Update()
     {
-        if(Input.GetKeyDown(_reloadKey))
-        {
-            SceneManager.LoadScene(_sceneName);
-        }
+        if (Input.GetKeyDown(_reloadKey))
+            RestartScene();
+    }
+
+    private void RestartScene()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
